4394c89b4d5fa3e394d4ef699c5bf58dc13065ac
joao vitor <joaovitorcastrosantos44@gmail.com>
2021-12-14 01:13:39 -0300

    exer. prontos

---
+++ variosCalc.js
@@ -0,0 +1,34 @@
+let numero = 10.5
+
+function absoluto(numero){
+    return numero
+}
+
+function inteiro(numero){
+    return Math.floor(numero)
+}
+function proxInteiro(numero){
+    return Math.round(numero)
+}
+function raizQ(numero){
+    return Math.sqrt(numero)
+}
+function raizC(numero){
+    return Math.cbrt(numero)
+}
+function elev2(numero){
+    return Math.pow(numero, 2)
+}
+function elev3(numero){
+    return Math.pow(numero, 3)
+}
+
+
+console.log(absoluto(numero))
+console.log(inteiro(numero))
+console.log(proxInteiro(numero))
+console.log(raizQ(numero))
+console.log(raizC(numero))
+console.log(elev2(numero))
+console.log(elev3(numero))
+
