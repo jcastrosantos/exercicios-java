50fa995c4f3e410d2c553e1b4b5c373d81c5ff76
joao vitor <joaovitorcastrosantos44@gmail.com>
2021-12-14 22:29:42 -0300

    exercicio 10

---
+++ Exc10/script.js
@@ -0,0 +1,15 @@
+let resultado = document.getElementById("resultado");
+
+function acao1(){
+    resultado.innerHTML += `<p>Clicou no primeiro botão</p>`
+
+}
+function acao2(){
+    resultado.innerHTML += `<p>Clicou no segundo botão</p>`
+}
+function acao3(){
+    resultado.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+function acao4(){
+    resultado.innerHTML += `<p>Clicou no quarto botão</p>`
+}
