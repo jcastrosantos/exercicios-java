7dba04775cf7cf5ab7b1c3fcd9b94de3b6b14fdc
joao vitor <joaovitorcastrosantos44@gmail.com>
2021-12-20 20:13:30 -0300

    maior valor concluido

--- condicoes_em_java/maiorValor.js
+++ condicoes_em_java/maiorValor.js
@@ -1,13 +1,10 @@
-let n1 = 10;
-let n2 = 15;
-
-// function mValor(n1,n2) {
-  
-// }
-if(n1 >= n2){
-    console.log(n1)
+function mValor(n1, n2) {
+if(n1 > n2){
+    console.log(`Analizando os valores ${n1} e ${n2} ,o maior é ${n1}`)
 }else{
-    return console.log(n2)
+    console.log(`Analizando os valores ${n1} e ${n2} ,o maior é ${n2}`)
 }
 
+}
 
+console.log(mValor(10, 15))
