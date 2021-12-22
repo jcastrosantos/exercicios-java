6b3ba4c8241d1d6b54138b49187801acd3f5e412
joao vitor <joaovitorcastrosantos44@gmail.com>
2021-12-22 18:05:02 -0300

    ex19

---
+++ condicoes_em_java/multA.js
@@ -0,0 +1,21 @@
+let n1 = 10
+let n2 = 10
+let opc = [som(n1, n2), sub(n1, n2), mult(n1, n2), div(n1, n2)]
+
+function som(n1, n2) {
+    return n1 + n2
+
+}
+function sub(n1, n2) {
+    return n1 - n2
+
+}
+function mult(n1, n2) {
+    return n1 * n2
+
+}
+function div(n1, n2) {
+    return n1 / n2
+
+}
+console.log(opc[3])
