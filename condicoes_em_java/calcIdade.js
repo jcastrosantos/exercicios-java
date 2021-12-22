a904c55f6f2932a44c2eb6aadaf0926d9bf19499
joao vitor <joaovitorcastrosantos44@gmail.com>
2021-12-22 17:03:18 -0300

    ex16

---
+++ condicoes_em_java/calcIdade.js
@@ -0,0 +1,11 @@
+let d = new Date
+let anoA = d.getFullYear()
+let n2 = 1997
+
+function calcIdade() {
+    return  `Quem nasceu em ${n2} vai completar ${anoA - n2} em ${anoA} `
+
+
+}
+
+console.log(calcIdade())
